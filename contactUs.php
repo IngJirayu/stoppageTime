9fe9babb20a0ce82f1849d4659de8b1d7aaa689d
IngJirayu <ingapivi@gmail.com>
2021-08-09 04:19:04 +0700

    added contact page

---
+++ contactUs.php
@@ -0,0 +1,21 @@
+<?php
+    require "header.php";
+    require "navbar.php";
+?>
+
+<div class="announcementWrapper">
+    <div class="announcementSecondWrapper">
+        <div class="announcementHeader">
+           ช่องทางการติดต่อ
+        </div>  
+        <div class="announcementText"> 
+        กดลิ้งด้านล่างตามช่องทางที่สะดวกได้เลย
+        <br>
+        <br>
+        ผ่าน <a href="https://www.facebook.com/stoppageTime">facebook</a> 
+        <br>
+        <br>
+        ผ่าน <a href="https://lin.ee/0nbvBBp">ไลน์@ </a> คลิกที่ลิ้งค์หรือพิม @555knvoo
+        </div>
+    </div>    
+</div>
